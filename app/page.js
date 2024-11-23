7163baf0025adb015ee9b7c5a35754a540dc017e
Nishant Gupta <usefreeforwork@gmail.com>
2024-11-24 00:30:14 +0530

    c1

--- app/page.js
+++ app/page.js
@@ -12,17 +12,10 @@ export default function Home() {
     document.body.appendChild(script);
 
     script.onload = () => {
-      if (window.Telegram?.WebApp) {
-        const user = window.Telegram.WebApp.initDataUnsafe?.user;
-        if (user) {
-          setUserId(user.id); // Set user ID from Telegram
-          saveUserData(user.id); // Save user ID to backend
-        } else {
-          console.log("User ID not available.");
-          setUserId(123);
-          saveUserData(123);
-        }
-      }
+      const user = window.Telegram?.WebApp.initDataUnsafe?.user;
+      const userId = user?.id || 123; // Fallback user ID if not available
+      setUserId(userId);
+      saveUserData(userId);
     };
 
     script.onerror = () => {
@@ -32,25 +25,19 @@ export default function Home() {
     return () => {
       document.body.removeChild(script);
     };
-  }, []); // Empty dependency array ensures the effect only runs once on component mount
+  }, []);
 
   const saveUserData = async (userId) => {
     try {
       const res = await fetch("/api/saveUser", {
         method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          userId,
-        }),
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ userId }),
       });
 
       if (res.ok) {
         const data = await res.json();
         console.log("User data saved:", data);
-      } else if (res.status === 409) {
-        console.log("User already exists, no action needed");
       } else {
         console.error("Error saving user data");
       }
@@ -62,11 +49,9 @@ export default function Home() {
   return (
     <main>
       <h1 className="text-3xl font-bold underline">Telegram User ID</h1>
-      {userId ? (
-        <p>Your User ID: {userId}</p>
-      ) : (
-        <p>Loading user information...</p>
-      )}
+      <p>
+        {userId ? `Your User ID: ${userId}` : "Loading user information..."}
+      </p>
     </main>
   );
 }
